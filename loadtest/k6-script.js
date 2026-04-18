7a720131e7c31735c4d15ee42e8c3c9673d993bf
addithyaprasad <62824681+addithyaprasad@users.noreply.github.com>
2026-04-17 20:20:40 -0400

    Update k6-script.js

--- loadtest/k6-script.js
+++ loadtest/k6-script.js
@@ -23,22 +23,21 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  stages: [
+  /*stages: [
     { duration: '2m', target: 100 },   // ramp up
     { duration: '5m', target: 500 },   // steady load
     { duration: '2m', target: 0 },    // ramp down
-  ],
+  ],*/
+  vus: 300,
+  duration: '8m',
 };
 
 export default function () {
-  const res = http.get('http://shop-alb-1467212189.us-east-1.elb.amazonaws.com/');
-
-  http.get('http://shop-alb-1467212189.us-east-1.elb.amazonaws.com/');              // homepage
-
+  
   http.get('http://shop-alb-1467212189.us-east-1.elb.amazonaws.com/api/products');  // backend call
   
-  check(res, {
+  /*check(res, {
     'status is 200': (r) => r.status === 200,
-  });
+  });*/
 
 }
